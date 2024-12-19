198fad4986abb06b8874564dc5935d5b8db0b945
Debi7 <debi1@mail.ru>
2024-12-19 18:04:36 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './styles/main.css';
-import Game from './game.js';
-
-const canvasElement = document.getElementById('canvas');
-window.onload = () => {
-  new Game(canvasElement);
-};
--- src/tiles.js
+++
@@ -1,7 +0,0 @@
-export default class Tile {
-  constructor(value, x, y) {
-    this.value = value;
-    this.x = x;
-    this.y = y;
-  }
-}
